b557596e90e55bd21fccb2be11bf8fd3e7496235
ifked <ifkedorland15@gmail.com>
2020-02-18 21:53:44 +0200

    PEP 8 & Docstring added

--- Functions/metric_dictionary.py
+++ Functions/metric_dictionary.py
@@ -1,16 +1,15 @@
 import numpy as np
 import pandas as pd
 
-def dictionary_of_metrics(items):
 
+def dictionary_of_metrics(items):
+    """Input an array of numbers and return a dictionary with calculated metrics as values."""
     arr = np.array(items)
 
-    #create a dictionary
-    my_dict = {'mean': round(arr.mean(),2),
-                'median': round(np.median(items),2),
-                'var': round(np.var(items, ddof=1),2),
-                'std': round(np.std(items, ddof=1),2),
-                'min': round(arr.min(),2),
-                'max': round(arr.max(),2)}
-   
+    my_dict = {'mean': round(arr.mean(), 2),
+               'median': round(np.median(items), 2),
+               'var': round(np.var(items, ddof=1), 2),
+               'std': round(np.std(items, ddof=1), 2),
+               'min': round(arr.min(), 2),
+               'max': round(arr.max(), 2)}
     return my_dict
